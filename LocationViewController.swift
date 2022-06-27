b7dc1a2701134010d7690dcb52f916867b71759f
Sergey <bindasov.sergey@gmail.com>
2022-06-27 12:24:38 +0300

    Initial commit

---
+++ LocationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocationNetology
+//
+//  Created by Sergey on 27.06.2022.
+//
+
+import UIKit
+
+class LocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
